674ef42d4de9bef0afe853584126e252ca2384d2
Starbox <39715563+Starbox7@users.noreply.github.com>
2023-03-26 23:54:43 +0900

    Mar. 26. 2023 - 범준
    일부 추가 주석 작성
    프로젝트 구조 재정리
    코드 미세조정
    
    아직 고칠거 많음
    그래서 api 직관적이지 않아서 폴더 3개로 분리함.추후 통합 회의 필요

--- admin/controller/userController.js
+++ admin/controller/userController.js
@@ -1,3 +1,4 @@
+/** 컨트롤러에서 DB 영역 분리 작업 예정 */
 /** 보안 */
 import 'dotenv/config';
 
@@ -17,31 +18,6 @@ import axios from 'axios';
 const { MONGO_URI, SALT, SERVICE_ID, ACCESS_KEY, SECRET_KEY, FROM_PHONE } = process.env;
 
 const userController = {
-  //   initAdmin: async (req, res) => {
-  //     try {
-  //       // 몽고DB 연결
-  //       await connect(MONGO_URI);
-  //       // 비밀번호 해싱을 위한 솔트 생성
-  //       const salt = await genSalt(parseInt(SALT));
-  //       // 사용자가 제공한 비밀번호를 해싱
-  //       const hashedPassword = await hash('404', salt);
-  //       // 데이터 생성
-  //       const newAdmin = new Admin({
-  //         email: req.body.email,
-  //         password: hashedPassword,
-  //         phone: req.body.phone,
-  //       });
-  //       // 데이터 삽입
-  //       await newAdmin.save();
-  //       // 성공 시, 201 상태 코드와 함께 관리자 데이터를 응답으로 반환
-  //       res.status(SC.CREATED.status).json(SC.CREATED);
-  //     } catch (err) {
-  //       // 오류 발생 시, 500 상태 코드와 함께 에러 메시지 반환
-  //       //   res.status(SC.SERVER_ERROR.status).json(SC.SERVER_ERROR);
-  //       console.log(err);
-  //       res.status(SC.SERVER_ERROR.status).json(err);
-  //     }
-  //   },
   isAdmin: async (req, res, next) => {
     const to = req.params.phone;
     const authNum = makeAuthNum();
